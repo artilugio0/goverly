7a2845a92d367f601c643ae565da9e299526c062
artilugio0 <artilugio0@proton.me>
2025-01-13 14:45:39 -0300

    remove id generation and assignments in widget functions

--- main.go
+++ main.go
@@ -29,11 +29,10 @@ func main() {
 		newCircle(),
 		newText("Hoy: Browser overlay para OBS usando Golang + WebAssembly"),
 		newTodoList([]TodoListItem{
-			{"Migrate canvas to svg", true},
-			{"Separate each widget in its own function", true},
-			{"Todo list widget", true},
-			{"Timer widget", false},
-			{"Basic hot reload on recompilation", false},
+			{"Remover IDs random de widgets", true},
+			{"Fix longitud de todo list item", false},
+			{"Crear widget de timer", false},
+			{"Hot reload basico", false},
 		}),
 	}
 
@@ -55,26 +54,24 @@ func newId() string {
 
 func newCircle() Widget {
 	const cRadius int = 10
-
-	id := newId()
-	document := js.Global().Get("document")
 	x, y := 0, 0
 
+	document := js.Global().Get("document")
+
+	circle := document.Call("createElementNS", "http://www.w3.org/2000/svg", "circle")
+	circle.Call("setAttribute", "r", cRadius)
+	circle.Call("setAttribute", "r", cRadius)
+	circle.Call("setAttribute", "stroke", "lime")
+	circle.Call("setAttribute", "stroke-width", "4")
+	circle.Call("setAttribute", "fill", "yellow")
+
+	x, y = svgWidth/2, svgHeight/2
+
+	appended := false
 	return func(svg js.Value) {
-		circle := document.Call("getElementById", id)
-
-		if circle.IsNull() {
-			circle = document.Call("createElementNS", "http://www.w3.org/2000/svg", "circle")
-			circle.Set("id", id)
-
-			circle.Call("setAttribute", "r", cRadius)
-			circle.Call("setAttribute", "r", cRadius)
-			circle.Call("setAttribute", "stroke", "lime")
-			circle.Call("setAttribute", "stroke-width", "4")
-			circle.Call("setAttribute", "fill", "yellow")
+		if !appended {
 			svg.Call("appendChild", circle)
-
-			x, y = svgWidth/2, svgHeight/2
+			appended = true
 		}
 
 		x = (x + 11) % svgWidth
@@ -90,27 +87,25 @@ func newText(text string) Widget {
 	const textAreaHeight int = 109
 	const marginLeft = 50
 
-	id := newId()
+	document := js.Global().Get("document")
+
+	svgtext := document.Call("createElementNS", "http://www.w3.org/2000/svg", "text")
+	svgtext.Call("setAttribute", "font-family", "Courier New")
+	svgtext.Call("setAttribute", "fill", "white")
+	svgtext.Call("setAttribute", "font-size", strconv.Itoa(textSize))
+	svgtext.Call("setAttribute", "x", marginLeft)
+	svgtext.Call("setAttribute", "y", svgHeight-(textAreaHeight-textSize)/2)
+
+	textnode := document.Call("createTextNode", text)
+	svgtext.Call("appendChild", textnode)
+	appended := false
 
 	return func(svg js.Value) {
-		document := js.Global().Get("document")
-		svgtext := document.Call("getElementById", id)
-		if !svgtext.IsNull() {
-			return
+		if !appended {
+			svg.Call("appendChild", svgtext)
+			appended = true
+
 		}
-
-		svgtext = document.Call("createElementNS", "http://www.w3.org/2000/svg", "text")
-		svgtext.Set("id", id)
-
-		svgtext.Call("setAttribute", "font-family", "Courier New")
-		svgtext.Call("setAttribute", "fill", "white")
-		svgtext.Call("setAttribute", "font-size", strconv.Itoa(textSize))
-		svgtext.Call("setAttribute", "x", marginLeft)
-		svgtext.Call("setAttribute", "y", svgHeight-(textAreaHeight-textSize)/2)
-
-		textnode := document.Call("createTextNode", text)
-		svgtext.Call("appendChild", textnode)
-		svg.Call("appendChild", svgtext)
 	}
 }
 
@@ -123,94 +118,91 @@ func newTodoList(items []TodoListItem) Widget {
 	const x int = 1920 - width
 	const y int = 1080 - 850
 
-	id := newId()
+	document := js.Global().Get("document")
 
-	return func(svg js.Value) {
-		document := js.Global().Get("document")
-		todoList := document.Call("getElementById", id)
-		if !todoList.IsNull() {
-			return
-		}
+	g := document.Call("createElementNS", "http://www.w3.org/2000/svg", "g")
 
-		g := document.Call("createElementNS", "http://www.w3.org/2000/svg", "g")
-		g.Set("id", id)
+	title := document.Call("createElementNS", "http://www.w3.org/2000/svg", "text")
+	title.Call("setAttribute", "font-family", "Courier New")
+	title.Call("setAttribute", "fill", "white")
+	title.Call("setAttribute", "font-size", titleTextSize)
+	title.Call("setAttribute", "x", 0)
+	title.Call("setAttribute", "y", 0)
+	textnode := document.Call("createTextNode", "To Do:")
+	title.Call("appendChild", textnode)
+	g.Call("appendChild", title)
 
-		title := document.Call("createElementNS", "http://www.w3.org/2000/svg", "text")
-		title.Call("setAttribute", "font-family", "Courier New")
-		title.Call("setAttribute", "fill", "white")
-		title.Call("setAttribute", "font-size", titleTextSize)
-		title.Call("setAttribute", "x", 0)
-		title.Call("setAttribute", "y", 0)
-		textnode := document.Call("createTextNode", "To Do:")
-		title.Call("appendChild", textnode)
-		g.Call("appendChild", title)
+	itemY := titleTextSize + itemMarginBottom
+	for _, it := range items {
+		listItemText := it.Description
+		textFields := strings.Fields(listItemText)
 
-		itemY := titleTextSize + itemMarginBottom
-		for _, it := range items {
-			listItemText := it.Description
-			textFields := strings.Fields(listItemText)
-
-			curWidth := 0
-			lines := []string{}
-			index := 0
-			for i, f := range textFields {
-				if curWidth+len(f)*(textSize*70/100) > width-2 {
-					if curWidth == 0 {
-						index = i + 1
-						if len(lines) == 0 {
-							lines = append(lines, "- "+textFields[i])
-						} else {
-							lines = append(lines, "  "+textFields[i])
-						}
-						continue
-					}
-
-					curWidth = 0
-					line := strings.Join(textFields[index:i], " ")
-					index = i
-
+		curWidth := 0
+		lines := []string{}
+		index := 0
+		for i, f := range textFields {
+			if curWidth+len(f)*(textSize*70/100) > width-2 {
+				if curWidth == 0 {
+					index = i + 1
 					if len(lines) == 0 {
-						lines = append(lines, "- "+line)
+						lines = append(lines, "- "+textFields[i])
 					} else {
-						lines = append(lines, line)
+						lines = append(lines, "  "+textFields[i])
 					}
+					continue
 				}
 
-				curWidth += len(f) * textSize
+				curWidth = 0
+				line := strings.Join(textFields[index:i], " ")
+				index = i
+
+				if len(lines) == 0 {
+					lines = append(lines, "- "+line)
+				} else {
+					lines = append(lines, line)
+				}
 			}
 
-			if len(lines) == 0 {
-				lines = append(lines, "- "+it.Description)
-			} else {
-				lines = append(lines, strings.Join(textFields[index:], " "))
-			}
-
-			color := "white"
-			if it.Done {
-				color = "lime"
-			}
-
-			for _, line := range lines {
-
-				itemText := document.Call("createElementNS", "http://www.w3.org/2000/svg", "text")
-				itemText.Call("setAttribute", "font-family", "Courier New")
-				itemText.Call("setAttribute", "fill", color)
-				itemText.Call("setAttribute", "font-size", strconv.Itoa(textSize))
-				itemText.Call("setAttribute", "x", 0)
-				itemText.Call("setAttribute", "y", itemY)
-
-				textnode := document.Call("createTextNode", line)
-
-				itemText.Call("appendChild", textnode)
-				g.Call("appendChild", itemText)
-
-				itemY += textSize + itemMarginBottom
-			}
+			curWidth += len(f) * textSize
 		}
 
-		g.Call("setAttribute", "transform", fmt.Sprintf("translate(%d, %d)", x, y))
+		if len(lines) == 0 {
+			lines = append(lines, "- "+it.Description)
+		} else {
+			lines = append(lines, strings.Join(textFields[index:], " "))
+		}
 
-		svg.Call("appendChild", g)
+		color := "white"
+		if it.Done {
+			color = "lime"
+		}
+
+		for _, line := range lines {
+
+			itemText := document.Call("createElementNS", "http://www.w3.org/2000/svg", "text")
+			itemText.Call("setAttribute", "font-family", "Courier New")
+			itemText.Call("setAttribute", "fill", color)
+			itemText.Call("setAttribute", "font-size", strconv.Itoa(textSize))
+			itemText.Call("setAttribute", "x", 0)
+			itemText.Call("setAttribute", "y", itemY)
+
+			textnode := document.Call("createTextNode", line)
+
+			itemText.Call("appendChild", textnode)
+			g.Call("appendChild", itemText)
+
+			itemY += textSize + itemMarginBottom
+		}
+	}
+
+	g.Call("setAttribute", "transform", fmt.Sprintf("translate(%d, %d)", x, y))
+
+	appended := false
+	return func(svg js.Value) {
+		if !appended {
+			svg.Call("appendChild", g)
+			appended = true
+		}
 	}
 }
 
